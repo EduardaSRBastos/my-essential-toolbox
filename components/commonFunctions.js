0f06c966149ca6658466d278d43e45f22a076114
Eduarda Bastos <eduarda.bastos@polarising.com>
2025-02-25 10:30:54 +0000

    Update files

---
+++ components/commonFunctions.js
@@ -0,0 +1,22 @@
+function restrictInput(event) {
+  const inputValue = event.target.value;
+
+  if (event.ctrlKey && event.key.length === 1) {
+    return;
+  }
+
+  if (
+    ["Backspace", "Delete", "ArrowLeft", "ArrowRight", "Tab"].includes(
+      event.key
+    )
+  )
+    return;
+
+  if (event.key === "." && inputValue.includes(".")) {
+    event.preventDefault();
+  }
+
+  if (!/[0-9.]/.test(event.key)) {
+    event.preventDefault();
+  }
+}
